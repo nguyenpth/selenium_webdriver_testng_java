626904726b098fbd7a5be6eda820ae2754cc0fdd
nguyen <nguyen@localhost>
2022-10-18 21:32:46 +0700

    add topic 7. 8

---
+++ src/webDriver/Topic_08_Custom_Dropdown.java
@@ -0,0 +1,59 @@
+package webDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_08_Custom_Dropdown {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName =System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac")) {
+			// mac 
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+		else {
+			//window 
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01_() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		
+		driver.quit();
+	}
+	
+	//sleep cứng ()
+	@Test
+	public void TC_02_() throws InterruptedException  {
+		Thread.sleep(5000);
+	
+	}
+	public void sleepInSecond(long timeInSecond ) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+}
